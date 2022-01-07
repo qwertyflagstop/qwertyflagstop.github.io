eb310fbff6219b1d6a85dd267d848663618e8882
Nicholas Peretti <nicholasperetti@gmail.com>
2022-01-07 00:47:38 -0700

    cards

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 let fontRegular
+let px = 0;
+let py = 0;
+let dx = 3.9;
+let dy = 0;
+let boxWidth = 0;
+let boxHeight = 0;
+let bString = "Happy\nBirthday\nKatie"
 
 function preload() {
     fontRegular = loadFont('/fonts/Roboto-Medium.ttf');
@@ -13,13 +20,38 @@ function setup() {
     stroke(0, 0, 0)
     textSize(36);
     textFont(fontRegular);
+    boxWidth = textWidth("Birthday");
+    boxHeight = 36*3;
+    px = 20;
+    py = 20;
+
 }
 
 function draw() {
-    if (mouseX > 10) {
+    // if (mouseX > 1) {
+    //     fill(frameCount % 1000 / 1000.0, 0.9, 0.9);
+    //     text("Happy\nBirthday\nKatie", mouseX - boxWidth / 2.0, mouseY - boxHeight);
+    //     px = mouseX;
+    //     py = mouseY;
+    // } else {
         fill(frameCount % 1000 / 1000.0, 0.9, 0.9);
-        text("Happy\nBirthday\nKatie", mouseX-50, mouseY-100);
-    }
+        text("Happy\nBirthday\nKatie", px - boxWidth / 2.0, py - boxHeight);
+        px += dx;
+        py += dy;
+        dy+= 0.2;
+        if (py>windowHeight){
+            dy = -dy*0.7;
+            py=windowHeight;
+        }
+        if (px>windowWidth) {
+            px = 10;
+            py = 10;
+            dx = Math.random() * 5.0 + 1.2;
+
+        }
+    //     }
+    // }
+
 }
 
 function windowResized() {
