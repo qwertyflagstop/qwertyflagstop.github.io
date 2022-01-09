725d1cc4fc93a6ace6ba37553d3eabc2f6fb3323
Nicholas Peretti <nicholasperetti@gmail.com>
2022-01-09 00:58:41 -0700

    new stuff

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let fontRegular
 let px = 0;
 let py = 0;
-let dx = 3.9;
-let dy = 0;
+let dx = 25.0;
+let dy = 25.0;
 let boxWidth = 0;
 let boxHeight = 0;
 let bString = "Happy\nBirthday\nKatie"
@@ -17,42 +17,67 @@ function setup() {
     colorMode(HSB, 1.0);
     background(255);
     strokeWeight(2);
-    stroke(0, 0, 0)
-    textSize(36);
+    stroke(0, 0.0, 0.9)
+    textSize(46);
     textFont(fontRegular);
     boxWidth = textWidth("Birthday");
-    boxHeight = 36*3;
-    px = 20;
-    py = 20;
+    boxHeight = 56;
+    px = windowWidth / 2.0;
+    py = windowHeight / 2.0;
 
 }
 
 function draw() {
-    // if (mouseX > 1) {
-    //     fill(frameCount % 1000 / 1000.0, 0.9, 0.9);
-    //     text("Happy\nBirthday\nKatie", mouseX - boxWidth / 2.0, mouseY - boxHeight);
-    //     px = mouseX;
-    //     py = mouseY;
-    // } else {
-        fill(frameCount % 1000 / 1000.0, 0.9, 0.9);
-        text("Happy\nBirthday\nKatie", px - boxWidth / 2.0, py - boxHeight);
-        px += dx;
-        py += dy;
-        dy+= 0.2;
-        if (py>windowHeight){
-            dy = -dy*0.7;
-            py=windowHeight;
-        }
-        if (px>windowWidth) {
-            px = 10;
-            py = 10;
-            dx = Math.random() * 5.0 + 1.2;
+    stroke(0, 0.0, 0.9)
+    fill(frameCount % 1000 / 1000.0, 0.7, 0.8);
+    rect(px - boxWidth / 2 - 20, py - (boxHeight * 3.7 / 2), boxWidth + 40, boxHeight * 3.5)
+    fill((frameCount + 500) % 1000 / 1000.0, 0.7, 0.8);
+    text("Happy\nBirthday\nKatie", px - boxWidth / 2.0, py - boxHeight);
+    px += dx;
+    py += dy;
+    if (py + boxHeight * 1.5 > windowHeight) {
+        dy = -dy;
+        py = windowHeight - boxHeight * 1.5
+    }
 
-        }
-    //     }
-    // }
+    if (py - boxHeight * 1.5 < 0) {
+        dy = -dy;
+        py = 0 + boxHeight * 1.5;
+    }
 
+    if (px > windowWidth - boxWidth / 2.0 || px - boxWidth / 2 < 0) {
+        dx = -dx;
+    }
+    let ms = 20.0;
+
+    if (mouseIsPressed) {
+        let jx = px - mouseX;
+        let jy = py - mouseY;
+        let dmag = Math.sqrt(jx * jx + jy * jy)
+
+        if (dmag > 30) {
+            dx -= jx * 0.025;
+            dy -= jy * 0.025;
+            stroke(1.0 - dmag / windowWidth * 0.5, 1.0, 1.0);
+            line(px, py, mouseX, mouseY);
+        }
+    }
+    let mag = Math.sqrt(dx * dx + dy * dy);
+    if (mag > ms) {
+        dx = dx / mag * ms * 0.99;
+        dy = dy / mag * ms * 0.99;
+    }
+    //console.log('X: ' + dx +' Y:' + dy);
 }
+
+
+// function mouseMoved() {
+//     let jx = px-mouseX;
+//     let jy = py-mouseY;
+//     dx -= jx*0.001;
+//     dy -= jy*0.001;
+//     console.log('clocked');
+// }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
