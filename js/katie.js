2d5c6ebbe29242d18525ba6c6b5e226ab5b01a35
Nicholas Peretti <nicholasperetti@gmail.com>
2022-01-17 01:02:14 -0700

    new fireworks

---
+++ js/katie.js
@@ -0,0 +1,83 @@
+let fontRegular
+let px = 0;
+let py = 0;
+let dx = 25.0;
+let dy = 25.0;
+let boxWidth = 0;
+let boxHeight = 0;
+let bString = "Happy\nBirthday\nKatie"
+
+function preload() {
+    fontRegular = loadFont('/fonts/Roboto-Medium.ttf');
+}
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    colorMode(HSB, 1.0);
+    background(255);
+    strokeWeight(2);
+    stroke(0, 0.0, 0.9)
+    textSize(46);
+    textFont(fontRegular);
+    boxWidth = textWidth("Birthday");
+    boxHeight = 56;
+    px = windowWidth / 2.0;
+    py = windowHeight / 2.0;
+
+}
+
+function draw() {
+    stroke(0, 0.0, 0.9)
+    fill(frameCount % 1000 / 1000.0, 0.7, 0.8);
+    rect(px - boxWidth / 2 - 20, py - (boxHeight * 3.7 / 2), boxWidth + 40, boxHeight * 3.5)
+    fill((frameCount + 500) % 1000 / 1000.0, 0.7, 0.8);
+    text("Happy\nBirthday\nKatie", px - boxWidth / 2.0, py - boxHeight);
+    px += dx;
+    py += dy;
+    if (py + boxHeight * 1.5 > windowHeight) {
+        dy = -dy;
+        py = windowHeight - boxHeight * 1.5
+    }
+
+    if (py - boxHeight * 1.5 < 0) {
+        dy = -dy;
+        py = 0 + boxHeight * 1.5;
+    }
+
+    if (px > windowWidth - boxWidth / 2.0 || px - boxWidth / 2 < 0) {
+        dx = -dx;
+    }
+    let ms = 20.0;
+
+    if (mouseIsPressed) {
+        let jx = px - mouseX;
+        let jy = py - mouseY;
+        let dmag = Math.sqrt(jx * jx + jy * jy);
+        if (dmag > 30) {
+            dx -= jx * 0.025;
+            dy -= jy * 0.025;
+            stroke(1.0 - dmag / windowWidth * 0.5, 1.0, 1.0);
+            line(px, py, mouseX, mouseY);
+        }
+    }
+    let mag = Math.sqrt(dx * dx + dy * dy);
+    if (mag > ms) {
+        dx = dx / mag * ms * 0.99;
+        dy = dy / mag * ms * 0.99;
+    }
+    //console.log('X: ' + dx +' Y:' + dy);
+}
+
+
+// function mouseMoved() {
+//     let jx = px-mouseX;
+//     let jy = py-mouseY;
+//     dx -= jx*0.001;
+//     dy -= jy*0.001;
+//     console.log('clocked');
+// }
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-let fontRegular
-let px = 0;
-let py = 0;
-let dx = 25.0;
-let dy = 25.0;
-let boxWidth = 0;
-let boxHeight = 0;
-let bString = "Happy\nBirthday\nKatie"
+let fireworks;
 
 function preload() {
     fontRegular = loadFont('/fonts/Roboto-Medium.ttf');
@@ -16,68 +9,109 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     colorMode(HSB, 1.0);
     background(255);
-    strokeWeight(2);
-    stroke(0, 0.0, 0.9)
-    textSize(46);
-    textFont(fontRegular);
-    boxWidth = textWidth("Birthday");
-    boxHeight = 56;
-    px = windowWidth / 2.0;
-    py = windowHeight / 2.0;
+    stroke(0, 0.0, 0.9);
+    strokeWeight(0);
+    fireworks = [];
+    addFirework(0, 0);
+    background(0, 0, 0, 1);
+
+
+}
+
+
+function newFirework(h, s, b, x, y, radius, decay_frames, num_lines, flame_size) {
+    let particles = [];
+    for (let i = 0; i < num_lines; i++) {
+        theta = i / num_lines * 4 * Math.PI + (Math.random()-0.5)*Math.PI;
+        px = x+Math.random()*10-5
+        py = y+Math.random()*10-5
+        particles.push({
+            x: x+Math.random()*10-5,
+            y: y+Math.random()*10-5,
+            dx: Math.cos(theta) * radius + Math.random()*0.1,
+            dy: Math.sin(theta) * radius + Math.random()*0.1,
+            last_x: px,
+            last_y: py,
+        })
+    }
+    return {
+        h: h,
+        s: s,
+        b: b,
+        t: 0,
+        decay_frames: decay_frames,
+        num_dots: 50,
+        num_lines: num_lines,
+        particles: particles,
+        particleWidth : flame_size
+    };
+}
+
+
+function addFirework(x, y) {
+    let h = Math.random();
+    fireworks.push(newFirework(h,
+        0.7,
+        1.0,
+        x,
+        y,
+        Math.random()*10+5,
+        60,
+        Math.random() * 10 + 30,
+        Math.random()*5.0));
+}
+
+function renderFirework(f) {
+    if (f.t < f.decay_frames) {
+        let prog = f.t/f.decay_frames;
+        strokeWeight(f.particleWidth);
+        for (const p of f.particles) {
+            stroke(f.h, f.s, f.b, 1.0-prog);
+            p.last_x = p.x;
+            p.last_y = p.y;
+            p.x += p.dx;
+            p.y += p.dy;
+            line(p.last_x, p.last_y, p.x, p.y);
+            p.dy += 0.2;
+            p.dx *= 0.99;
+            p.dy *= 0.99;
+        }
+        f.t += 1;
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function renderfireworks() {
+    offset = 0;
+    offsets_to_remove = [];
+    for (const f of fireworks) {
+        if (renderFirework(f)) {
+            offsets_to_remove.push(offset);
+        }
+        offset++;
+    }
+    for (const index of offsets_to_remove) {
+        fireworks.splice(index, 1);
+    }
 
 }
 
 function draw() {
-    stroke(0, 0.0, 0.9)
-    fill(frameCount % 1000 / 1000.0, 0.7, 0.8);
-    rect(px - boxWidth / 2 - 20, py - (boxHeight * 3.7 / 2), boxWidth + 40, boxHeight * 3.5)
-    fill((frameCount + 500) % 1000 / 1000.0, 0.7, 0.8);
-    text("Happy\nBirthday\nKatie", px - boxWidth / 2.0, py - boxHeight);
-    px += dx;
-    py += dy;
-    if (py + boxHeight * 1.5 > windowHeight) {
-        dy = -dy;
-        py = windowHeight - boxHeight * 1.5
-    }
+    renderfireworks();
+    fill(0, 0, 0, 0.1);
+    strokeWeight(0);
+    rect(0, 0, windowWidth, windowHeight);
 
-    if (py - boxHeight * 1.5 < 0) {
-        dy = -dy;
-        py = 0 + boxHeight * 1.5;
-    }
-
-    if (px > windowWidth - boxWidth / 2.0 || px - boxWidth / 2 < 0) {
-        dx = -dx;
-    }
-    let ms = 20.0;
-
-    if (mouseIsPressed) {
-        let jx = px - mouseX;
-        let jy = py - mouseY;
-        let dmag = Math.sqrt(jx * jx + jy * jy);
-        if (dmag > 30) {
-            dx -= jx * 0.025;
-            dy -= jy * 0.025;
-            stroke(1.0 - dmag / windowWidth * 0.5, 1.0, 1.0);
-            line(px, py, mouseX, mouseY);
-        }
-    }
-    let mag = Math.sqrt(dx * dx + dy * dy);
-    if (mag > ms) {
-        dx = dx / mag * ms * 0.99;
-        dy = dy / mag * ms * 0.99;
-    }
-    //console.log('X: ' + dx +' Y:' + dy);
 }
 
-
-// function mouseMoved() {
-//     let jx = px-mouseX;
-//     let jy = py-mouseY;
-//     dx -= jx*0.001;
-//     dy -= jy*0.001;
-//     console.log('clocked');
-// }
+function mouseClicked() {
+    addFirework(Math.random() * windowWidth * 0.9 + windowWidth * 0.1,
+        Math.random() * windowHeight * 0.5);
+}
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+    background(0, 0, 0, 1);
 }
